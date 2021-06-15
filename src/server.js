e162159fc68f42cd41a7db43ce4b493c5a69e64b
Muhammad Alvin Ali Fikri <alvin.geomatics@gmail.com>
2021-06-15 10:32:28 +0700

    added validator functionality

---
+++ src/server.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/songsService');
+const SongsValidator = require('./validation/songs')
+
+require('dotenv').config();
+
+const init = async () => {
+  const songsService = new SongsService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
