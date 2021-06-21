aa91fe3971c0899d37e3b3ff28b9541a9a8586b2
Muhammad Alvin Ali Fikri <alvin.geomatics@gmail.com>
2021-06-21 21:25:21 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 /* eslint-disable no-console */
 const Hapi = require('@hapi/hapi');
+
+// Songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/songsService');
 const SongsValidator = require('./validation/songs');
+
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UserValidator = require('./validation/users');
 
 require('dotenv').config();
 
 const init = async () => {
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -19,13 +26,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UserValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
